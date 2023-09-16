d827e8aad8b2b3bb8c84a8c1cfa29380bf2e4fce
KlausBighi <klaus.bighi@hotmail.com>
2023-09-16 18:10:58 +0200

    Finished

--- TestesUnitarios.Desafio.Console/Program.cs
+++ TestesUnitarios.Desafio.Console/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TestesUnitarios.Desafio.Console.Services;
+
+
+var lista = new List<int> { 5, -1, -8, 9 };
+//var resultadoEsperado = new List<int> { 5, 9 };
+
+ValidacoesLista v = new ValidacoesLista();
+v.RemoverNumerosNegativos(lista);
+
+//lista.ForEach(i => Console.Write("{0}\t", i));
